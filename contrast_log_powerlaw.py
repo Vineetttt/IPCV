ff8d7b7edb09a0f020bef954114c204a3c052fbe
Vineet Chotaliya <105866260+Vineetttt@users.noreply.github.com>
2024-05-15 21:08:07 +0530

    Update contrast_log_powerlaw.py

--- contrast_log_powerlaw.py
+++ contrast_log_powerlaw.py
@@ -1,9 +1,19 @@
-def contrast_stretching(img, r1, s1, r2, s2):
-    stretched_img = np.copy(img)
-    stretched_img[stretched_img < r1] = stretched_img[stretched_img < r1] * (s1 / r1)
-    stretched_img[(r1 <= stretched_img) & (stretched_img <= r2)] = ((stretched_img[(r1 <= stretched_img) & (stretched_img <= r2)] - r1) * ((s2 - s1) / (r2 - r1))) + s1
-    stretched_img[stretched_img > r2] = ((stretched_img[stretched_img > r2] - r2) * ((255 - s2) / (255 - r2))) + s2
-    return stretched_img.astype(np.uint8)
+def contrast_stretching(image,s1,s2,r1,r2):
+    height,width, channels= image.shape
+    output = image.copy()
+    alpha = s1/r1
+    beta = (s2 - s1)/(r2 - r1)
+    gamma = (255 - s2)/(255 - r2)
+    for i in range(height):
+        for j in range(width):
+            for c in range(channels):
+                if image[i,j,c] < r1:
+                    output[i,j,c] = alpha*image[i,j,c]
+                elif image[i,j,c] >= r1 and image[i,j,c] < r2:
+                    output[i,j,c] = beta*(image[i,j,c] - r1) + s1
+                else:
+                    output[i,j,c] = gamma*(image[i,j,c] - r2) + s2
+    return output
 
 def log_transformation(img):
     c = 255 / np.log(1 + np.max(img))
